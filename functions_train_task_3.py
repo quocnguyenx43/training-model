7af941dbdec93b1060866905e1dde3be7aa29684
quocnguyen <quocnguyenx43@gmail.com>
2024-06-05 22:49:17 +0700

    ad

--- functions_train_task_3.py
+++ functions_train_task_3.py
@@ -1,5 +1,5 @@
 import torch
-
+from tqdm import tqdm
 
 
 def train(model, optimizer, tokenizer, train_dataloader, saving_path=None, device='cpu'):
@@ -7,26 +7,26 @@ def train(model, optimizer, tokenizer, train_dataloader, saving_path=None, devic
     for epoch in range(5):
         model.train()
         running_loss = 0.0
-
-        print('epoch', epoch)
         
-        for _, batch in enumerate(train_dataloader, 0):
-            print('batch', batch)
-            ids = batch['input']['input_ids'].to(device, dtype=torch.long)
-            mask = batch['input']['attention_mask'].to(device, dtype=torch.long)
 
-            y = batch['label']['input_ids'].to(device, dtype=torch.long)
-            y_ids = y[:, :-1].contiguous()
-            lm_labels = y[:, 1:].clone().detach()
-            lm_labels[y[:, 1:] == tokenizer.pad_token_id] = -100
+        with tqdm(train_dataloader, desc=f"Epoch {epoch + 1}/{5}") as tqdm_loader:
+            for batch_idx, batch in enumerate(tqdm_loader):
+                print('batch', batch)
+                ids = batch['input']['input_ids'].to(device, dtype=torch.long)
+                mask = batch['input']['attention_mask'].to(device, dtype=torch.long)
 
-            outputs = model(input_ids=ids, attention_mask=mask, decoder_input_ids=y_ids, labels=lm_labels)
-            loss = outputs[0]
-            running_loss += loss
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                y = batch['label']['input_ids'].to(device, dtype=torch.long)
+                y_ids = y[:, :-1].contiguous()
+                lm_labels = y[:, 1:].clone().detach()
+                lm_labels[y[:, 1:] == tokenizer.pad_token_id] = -100
+
+                outputs = model(input_ids=ids, attention_mask=mask, decoder_input_ids=y_ids, labels=lm_labels)
+                loss = outputs[0]
+                running_loss += loss
+                
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
         if saving_path:
             pass

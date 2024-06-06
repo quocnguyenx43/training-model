8b248caad4b1d7749f77b4a6ec1c7eae4cdd233c
quocnguyen <quocnguyenx43@gmail.com>
2024-06-06 21:44:22 +0700

    test task 3

--- run_train_generation_task_finetune.py
+++ run_train_generation_task_finetune.py
@@ -28,3 +28,12 @@ func.train_task_3(
     device=imp.args['device']
 )
 
+print('Validating ...')
+a, b = func.validate_task_3(
+    generation_model, tokenizer,
+    imp.test_dataloader, target_len=imp.args['target_len'],
+    device=imp.args['device']
+)
+
+
+print(a, b)

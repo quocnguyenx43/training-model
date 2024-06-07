9654f86eb5ecd5e8d6e2c663ec93de2d02ff7956
quocnguyen <quocnguyenx43@gmail.com>
2024-06-07 09:11:34 +0700

    .sh file

--- commands.sh
+++ commands.sh
@@ -1,7 +1,12 @@
-@REM Runing file
-@echo off 
+#!/bin/bash
 
-echo ########### TRAINING (TASK-1) ###########
+
+echo "########### TRAINING (TASK-1) ###########"
+
+# Evaluation: vinai/phobert-base 
 python run_evaluation_cls_task.py --task "task-1" --model_type "simple" --model_name "vinai/phobert-base" --source_len 200 --batch_size 16
 python run_evaluation_cls_task.py --task "task-1" --model_type "simple" --model_name "uitnlp/visobert" --source_len 400 --batch_size 16
-echo All commands completed!
+
+
+
+echo "All commands completed!"

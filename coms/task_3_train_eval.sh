aed143a2e23055c9ef1fbb647b0fe06df7e87cc6
quocnguyen <quocnguyenx43@gmail.com>
2024-06-11 14:03:35 +0700

    task_3

--- coms/task_3_train_eval.sh
+++ coms/task_3_train_eval.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Redirect outputs
-exec > >(tee -i ./results/logs/train_eval_task_3.log) 2>&1
+exec > >(tee -i ./results/logs/task_3_train_eval.log) 2>&1
 
 
 # Training
 echo "Training VietAI/vit5-base"
-python run_train_generation_task.py --task "task-3" --model_name "VietAI/vit5-base" --source_len 1024 --target_len 512 --batch_size 4 --learning_rate 0.001 --epochs 3
+python run_train_generation_task.py --task "task-3" --model_name "VietAI/vit5-base" --source_len 768 --target_len 128 --batch_size 12 --learning_rate 0.001 --epochs 3
 
 echo "Evaluating VietAI/vit5-base"
-python run_evaluation_generation_task.py --task "task-3" --model_name "VietAI/vit5-base" --source_len 1024 --target_len 512 --batch_size 4
+python run_evaluation_generation_task.py --task "task-3" --model_name "VietAI/vit5-base" --source_len 768 --target_len 128 --batch_size 12
 
 echo "All commands completed!"

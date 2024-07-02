95266b72bb5d55fa5b383a988083a52f5f5658b5
quocnguyen <quocnguyenx43@gmail.com>
2024-07-02 18:55:08 +0700

    add

--- run_prediction_full.py
+++ run_prediction_full.py
@@ -165,20 +165,20 @@ df1 = pd.DataFrame(predictions_1, columns=['predicted_label'])
 df2 = pd.DataFrame(predictions_2, columns=['predicted_title', 'predicted_desc', 'predicted_comp', 'predicted_other'])
 df_merged = pd.concat([df1, df2], axis=1)
 # df_merged.to_csv('results/' + task_1_model_path.replace('.', '_').replace('/', '_') + task_2_model_path.replace('.', '_').replace('/', '_') + '.csv')
-df_merged.predicted_label = df_merged.predicted_label.map(mapping_label)
-df_merged.predicted_title = df_merged.predicted_title.map(mapping_aspect)
-df_merged.predicted_desc = df_merged.predicted_desc.map(mapping_aspect)
-df_merged.predicted_comp = df_merged.predicted_comp.map(mapping_aspect)
-df_merged.predicted_other = df_merged.predicted_other.map(mapping_aspect)
+# df_merged.predicted_label = df_merged.predicted_label.map(mapping_label)
+# df_merged.predicted_title = df_merged.predicted_title.map(mapping_aspect)
+# df_merged.predicted_desc = df_merged.predicted_desc.map(mapping_aspect)
+# df_merged.predicted_comp = df_merged.predicted_comp.map(mapping_aspect)
+# df_merged.predicted_other = df_merged.predicted_other.map(mapping_aspect)
 
 def adding_previous_tasks(df):
     previous_task_outputs = []
     for index in range(len(df)): 
-        s = "khía cạnh tiêu đề: " + df.iloc[index]['predicted_title'] + " [SEP] " \
-            + "khía cạnh mô tả: " + df.iloc[index]['predicted_desc'] + " [SEP] " \
-            + "khía cạnh công ty: " + df.iloc[index]['predicted_comp'] + " [SEP] " \
-            + "khía cạnh khác: " + df.iloc[index]['predicted_other'] + " [SEP] " \
-            + "nhãn chung: " + df.iloc[index]['predicted_label']  + " [SEP] "
+        s = "khía cạnh tiêu đề: " + mapping_aspect[df.iloc[index]['predicted_title']] + " [SEP] " \
+            + "khía cạnh mô tả: " + mapping_aspect[df.iloc[index]['predicted_desc']] + " [SEP] " \
+            + "khía cạnh công ty: " + mapping_aspect[df.iloc[index]['predicted_comp']] + " [SEP] " \
+            + "khía cạnh khác: " + mapping_aspect[df.iloc[index]['predicted_other']] + " [SEP] " \
+            + "nhãn chung: " + mapping_label[df.iloc[index]['predicted_label']]  + " [SEP] "
         
         previous_task_outputs.append(s[:-1])
 

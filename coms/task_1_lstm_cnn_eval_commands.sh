d92dba7ffea268683e68deb62cb40ae8466c687a
quocnguyen <quocnguyenx43@gmail.com>
2024-07-03 11:45:18 +0700

    change lstm num_layers

--- coms/task_1_lstm_cnn_eval_commands.sh
+++ coms/task_1_lstm_cnn_eval_commands.sh
@@ -8,22 +8,22 @@ exec > >(tee -i ./results/logs_raw/task_1_lstm_cnn_eval_log.log) 2>&1
 echo "Evaluating LSTM"
 
 echo "lstm + vinai/phobert-base"
-python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 1 --model_name "vinai/phobert-base" --source_len 256 --batch_size 32
+python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 4 --model_name "vinai/phobert-base" --source_len 256 --batch_size 32
 
 echo "lstm + uitnlp/visobert"
-python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 1 --model_name "uitnlp/visobert" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 4 --model_name "uitnlp/visobert" --source_len 512 --batch_size 32
 
 echo "lstm + uitnlp/CafeBERT"
-python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 1 --model_name "uitnlp/CafeBERT" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 4 --model_name "uitnlp/CafeBERT" --source_len 512 --batch_size 32
 
 echo "lstm + xlm-roberta-base"
-python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 1 --model_name "xlm-roberta-base" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 4 --model_name "xlm-roberta-base" --source_len 512 --batch_size 32
 
 echo "lstm + bert-base-multilingual-cased"
-python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 1 --model_name "bert-base-multilingual-cased" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 4 --model_name "bert-base-multilingual-cased" --source_len 512 --batch_size 32
 
 echo "lstm + distilbert-base-multilingual-cased"
-python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 1 --model_name "distilbert-base-multilingual-cased" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-1" --model_type "lstm" --hidden_size 128 --num_layers 4 --model_name "distilbert-base-multilingual-cased" --source_len 512 --batch_size 32
 
 
 

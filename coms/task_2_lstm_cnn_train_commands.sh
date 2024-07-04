b245188a8cdf4a1a8558cdcc2d38c6b59148942d
quocnguyen <quocnguyenx43@gmail.com>
2024-07-05 00:07:19 +0700

    add

--- coms/task_2_lstm_cnn_train_commands.sh
+++ coms/task_2_lstm_cnn_train_commands.sh
@@ -31,19 +31,19 @@ exec > >(tee -i ./results/logs_raw/task_2_lstm_cnn_train_log.log) 2>&1
 echo "Training CNN"
 
 echo "cnn + vinai/phobert-base"
-python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 256 --kernel_size 128 --padding 64 --model_name "vinai/phobert-base" --source_len 256 --batch_size 32 --learning_rate 0.001 --epochs 20
+python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "vinai/phobert-base" --source_len 256 --batch_size 32 --learning_rate 0.001 --epochs 20
 
 echo "cnn + uitnlp/visobert"
-python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 256 --kernel_size 128 --padding 64 --model_name "uitnlp/visobert" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
+python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "uitnlp/visobert" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
 
 echo "cnn + uitnlp/CafeBERT"
-python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 256 --kernel_size 128 --padding 64 --model_name "uitnlp/CafeBERT" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
+python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "uitnlp/CafeBERT" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
 
 echo "cnn + xlm-roberta-base"
-python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 256 --kernel_size 128 --padding 64 --model_name "xlm-roberta-base" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
+python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "xlm-roberta-base" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
 
 echo "cnn + bert-base-multilingual-cased"
-python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 256 --kernel_size 128 --padding 64 --model_name "bert-base-multilingual-cased" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
+python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "bert-base-multilingual-cased" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
 
 echo "cnn + distilbert-base-multilingual-cased"
-python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 256 --kernel_size 128 --padding 64 --model_name "distilbert-base-multilingual-cased" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20
+python run_train_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "distilbert-base-multilingual-cased" --source_len 512 --batch_size 32 --learning_rate 0.001 --epochs 20

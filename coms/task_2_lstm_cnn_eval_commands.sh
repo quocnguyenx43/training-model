e36f3596d7e623d0da15032a56eb9b8ef4e55315
quocnguyen <quocnguyenx43@gmail.com>
2024-07-05 10:14:45 +0700

    add

--- coms/task_2_lstm_cnn_eval_commands.sh
+++ coms/task_2_lstm_cnn_eval_commands.sh
@@ -30,19 +30,19 @@ exec > >(tee -i ./results/logs_raw/task_2_lstm_cnn_eval_log.log) 2>&1
 echo "Evaluating CNN"
 
 echo "cnn + vinai/phobert-base"
-python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "vinai/phobert-base" --source_len 256 --batch_size 32
+python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 64 --kernel_size 64 --padding 64 --model_name "vinai/phobert-base" --source_len 256 --batch_size 32
 
 echo "cnn + uitnlp/visobert"
-python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "uitnlp/visobert" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 64 --kernel_size 64 --padding 64 --model_name "uitnlp/visobert" --source_len 512 --batch_size 32
 
 echo "cnn + uitnlp/CafeBERT"
-python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "uitnlp/CafeBERT" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 64 --kernel_size 64 --padding 64 --model_name "uitnlp/CafeBERT" --source_len 512 --batch_size 32
 
 echo "cnn + xlm-roberta-base"
-python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "xlm-roberta-base" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 64 --kernel_size 64 --padding 64 --model_name "xlm-roberta-base" --source_len 512 --batch_size 32
 
 echo "cnn + bert-base-multilingual-cased"
-python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "bert-base-multilingual-cased" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 64 --kernel_size 64 --padding 64 --model_name "bert-base-multilingual-cased" --source_len 512 --batch_size 32
 
 echo "cnn + distilbert-base-multilingual-cased"
-python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 128 --kernel_size 128 --padding 64 --model_name "distilbert-base-multilingual-cased" --source_len 512 --batch_size 32
+python run_evaluation_cls_task.py --task "task-2" --model_type "cnn" --num_channels 64 --kernel_size 64 --padding 64 --model_name "distilbert-base-multilingual-cased" --source_len 512 --batch_size 32
